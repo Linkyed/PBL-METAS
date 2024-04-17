39087a3cca225a4a58d998cc52ace937cccacdae
Pedro Henrique A. <phaalmeida1@gmail.com>
2024-04-17 01:59:07 -0300

    Evento do mouse usando o arquivo MICE

--- testar_mouse_0.c
+++ testar_mouse_0.c
@@ -1,36 +1,50 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <linux/input.h>
+#include <stdlib.h>
+int main(int argc, char** argv)
+{
+    int fd, bytes;
+    unsigned char data[3];
 
-//CODIGO PARA TESTAR CLIQUE DO MOUSE
+    const char *pDevice = "/dev/input/mice";
 
-#define MOUSEFILE "/dev/input/event0" // This might vary depending on your system
-
-int main() {
-    int fd;
-    struct input_event ie;
-
-    if ((fd = open(MOUSEFILE, O_RDONLY)) == -1) {
-        perror("Cannot open mouse device");
-        return 1;
+    // Open Mouse
+    fd = open(pDevice, O_RDWR);
+    if(fd == -1)
+    {
+        printf("ERROR Opening %s\n", pDevice);
+        return -1;
     }
 
-    printf("Reading from %s...\n", MOUSEFILE);
+    int left, middle, right;
+    int cord_X = 0;
+    int cord_Y = 0;
+    signed char x, y;
+    while(1)
+    {
+        // Read Mouse     
+        bytes = read(fd, data, sizeof(data)); // Abre o arquivo MICE para leitura dos eventos de input que o mouse esta enviando
 
-    while (1) {
-        if (read(fd, &ie, sizeof(struct input_event)) != sizeof(struct input_event)) {
-            perror("Error reading");
-            return 1;
-        }
-        
-        // Check if event is a mouse button click event
-        if (ie.type == EV_KEY && ie.code == BTN_LEFT && ie.value == 1) {
-            printf("Left mouse button clicked!\n");
-            sleep(1); // Sleep for 1 second
-        }
+        if(bytes > 0)
+        {
+            left = data[0] & 0x1; // Lê o 1º LSB e se for igual a 1 significa que o botão esquerdo foi pressionado então left = 1 tambem
+            right = data[0] & 0x2; // Lê o 2º LSB e se for igual a 1 significa que o botão direito foi pressionado então right = 1 tambem
+            middle = data[0] & 0x4;// Lê o 4º LSB e se for igual a 1 significa que o botão do meio foi pressionado então middle = 1 tambem
+
+            x = data[1]; // Lê o segundo pack que é responsavel por indicar a movimentação do mouse no eixo X
+            y = data[2]; // Lê o terceiro pack que é responsavel por indicar a movimentação do mouse no eixo Y
+            //printf("x=%d, y=%d, btn_esqueda=%d, btn_meio=%d, btn_direito=%d\n", x, y, left, middle, right);
+            if ( (cord_X+1 < 300 && cord_X-1 > 0)){
+                 if (x > 0){
+                        cord_X += 1;}
+                 if (x < 0){
+                        cord_X -= 1;}
+            }
+
+            printf("Coordenada X: %d\n", cord_X);
+            system("clear");
+        }   
     }
-
-    close(fd);
-    return 0;
+    return 0; 
 }
